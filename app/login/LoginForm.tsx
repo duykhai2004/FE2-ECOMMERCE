a3ce6d6e17a5e7edf74f2bc7ada92e6f5e95ccc6
duykhai2004 <22211tt3708@mail.tdc.edu.vn>
2024-05-28 01:16:35 +0700

    Connect MongoDB, Register

---
+++ app/login/LoginForm.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { useState } from "react";
+import Input from "../components/inputs/Input";
+import Heading from "../components/products/Heading";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import Button from "../components/products/Button";
+import Link from "next/link";
+import { AiOutlineGoogle } from "react-icons/ai";
+
+const LoginForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    setIsLoading(true);
+    console.log(data);
+  };
+
+  return (
+    <>
+      <Heading title="Sign in to Shop" center />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={AiOutlineGoogle}
+        onClick={() => {}}
+      />
+      <hr className="bg-slate-300 w-full h-px" />
+
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+
+      <Input
+        id="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+        type="password"
+      />
+      <Button
+        label={isLoading ? "Loading" : "Login"}
+        onClick={handleSubmit(onSubmit)}
+      />
+      <p>
+        Do not have an account ?
+        <Link className="underline" href="/register">
+          Sign Up
+        </Link>
+      </p>
+    </>
+  );
+};
+
+export default LoginForm;
---
+++ app/register/RegisterForm.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import { useState } from "react";
+import Input from "../components/inputs/Input";
+import Heading from "../components/products/Heading";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import Button from "../components/products/Button";
+import Link from "next/link";
+import { AiOutlineGoogle } from "react-icons/ai";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+const RegisterForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const router = useRouter();
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    setIsLoading(true);
+    //console.log(data);
+    axios.post("/api/register", data).then(() => {
+      toast.success("Account created successfully");
+      signIn("credentials", {
+        email: data.email,
+        password: data.password,
+        redirect: false,
+      }).then((callback) => {
+        if (callback?.ok) {
+          router.push('/cart')
+          router.refresh(
+            
+          );
+          toast.success("Logged")
+        }
+        if (callback?.error) {
+          toast.error(callback.error)
+        }
+      });
+    }).catch(()=> {
+      toast.error("Something went wrong");
+    }).finally(() => {
+      setIsLoading(false);
+    });
+  };
+
+  return (
+    <>
+      <Heading title="Register" center />
+      <Button
+        outline
+        label="Sign Up with Google"
+        icon={AiOutlineGoogle}
+        onClick={() => {}}
+      />
+      <hr className="bg-slate-300 w-full h-px" />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+
+      <Input
+        id="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+        type="password"
+      />
+      <Button
+        label={isLoading ? "Loading" : "Sign Up"}
+        onClick={handleSubmit(onSubmit)}
+      />
+      <p>
+        Already have an account ?
+        <Link className="underline" href="/login">
+          Log in
+        </Link>
+      </p>
+    </>
+  );
+};
+
+export default RegisterForm;
